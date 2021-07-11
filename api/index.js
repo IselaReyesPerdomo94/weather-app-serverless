e20058b31e5d0f297a274ad4729cd6f1dfb0a999
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2021-07-11 11:48:12 -0700

    Changing function name

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const fetch = require('cross-fetch');
 const allowCors = require('../utils/allowCors');
 
-const fetchWeather = async(req, res) => {
+const fetchWeatherBoston = async(req, res) => {
     const response = await fetch('https://www.metaweather.com/api/location/2367105/');
     const data = await response.json();
     res.json(data);
 }
 
-module.exports = allowCors(fetchWeather);
+module.exports = allowCors(fetchWeatherBoston);
